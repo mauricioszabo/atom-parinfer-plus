090e388a52d5b94ae20d9747d79cb2e031167998
Maurício Szabo <mauricio.szabo@gmail.com>
2020-09-01 10:26:52 -0300

    First version of Parinfer working

--- src/parinfer_plus/core.cljs
+++ src/parinfer_plus/core.cljs
@@ -8,39 +8,37 @@
   (def paren-mode (.-parenMode wrapper))
   (def smart-mode (.-smartMode wrapper)))
 
-(defn- run-parinfer! [^js buffer, ^js changes]
-  (let [new-range (.-newRange changes)
-        old-range (.-oldRange changes)
-        new-code (.getText buffer)
-        old-code (-> (TextBuffer. #js {:text new-code})
-                     (doto (.setTextInRange new-range
-                                            (.-oldText changes)))
-                     .getText)
-        res ^js (smart-mode new-code
-                            #js {:prevText old-code})]
-                                 ; :cursorLine (.. new-range -end -row)
-                                 ; :prevCursorLine (.. old-range -end -row)
-                                 ; :cursorX (.. new-range -end -col)
-                                 ; :prevCursorX (.. old-range -end -col)})]
-    (println :OLD)
-    (println old-code)
-    (println :NEW)
-    (println new-code)
-    (println
-     (.-text res)
-     (.-success res))))
+(defn- run-parinfer! [^js editor, can-change?, ^js changes]
+  (when @can-change?
+    (let [buffer ^js (.getBuffer editor)
+          new-range (.-newRange changes)
+          old-range (.-oldRange changes)
+          new-code (.getText buffer)
+          old-code (-> (TextBuffer. #js {:text new-code})
+                       (doto (.setTextInRange new-range
+                                              (.-oldText changes)))
+                       .getText)
+          res ^js (smart-mode new-code
+                              #js {:prevText old-code
+                                   :cursorLine (.. new-range -end -row)
+                                   :prevCursorLine (.. old-range -end -row)
+                                   :cursorX (.. new-range -end -column)
+                                   :prevCursorX (.. old-range -end -column)})
+          new-text (.-text res)]
 
-    ; (.setTextInRange old-code new-range old-txt)))
-    ; (println
-    ;  (str/join "\n" (.getLines old-code)))
-    ; (let [old-txt (.-oldText changes)])
-    ;
-    ; (def changes changes)))
-  ; (tap> (.-changes changes)))
+      (def new-range new-range)
+      (when (.-success res)
+        (reset! can-change? false)
+        (when-not (= new-text new-code)
+          (.setText editor (.-text res))
+          (.setCursorBufferPosition editor #js [(.-cursorLine res) (.-cursorX res)])
+          (.groupLastChanges buffer)))))
+  (reset! can-change? true))
 
 (defn- observe-editor [^js editor]
-  (let [buffer (.getBuffer editor)]
-    (aux/subscribe! (. buffer (onDidChange #(run-parinfer! buffer %))))))
+  (let [can-change? (atom true)]
+    (aux/subscribe! (.. editor getBuffer
+                        (onDidChange #(run-parinfer! editor can-change? %))))))
 
 (defn main []
   (aux/subscribe! (.. js/atom -workspace (observeTextEditors observe-editor))))
